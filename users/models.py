eeda6fcd47d9de2e5da6f8851bb11017d314d121
alexkmerz <alexkarlmerz@gmail.com>
2019-03-12 22:23:25 +1300

    Added user model and migration

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField("First Name", max_length=255)
+    last_name = models.CharField("Last Name", max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    created_at = models.DateTimeField("Created At", auto_now_add=True)
+    last_log_in = models.DateTimeField("Last Log In", auto_now_add=False)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
